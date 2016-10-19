bdbd4e8f88204b49a4cb7e081b3fcd2c0b93cfb6
sara <sara@huhbub.com>
2016-10-19 19:23:38 +0100

    completed ch.5/ex.1

---
+++ ch.5/ex1.rb
@@ -0,0 +1,9 @@
+#full name greeting
+
+puts 'Yo! What\'s your first name?'
+firstname = gets.chomp
+puts 'Awesome! And your middle name?'
+middlename = gets.chomp
+puts 'Wow! How about your last name?'
+lastname = gets.chomp
+puts 'I\'m absolutely thrilled to meet you, ' + firstname + ' ' + middlename + ' ' + lastname + '!'
