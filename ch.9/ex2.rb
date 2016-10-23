f8c8b1bba2e2e8867ef28920c4ba9aa19c9373f8
sara <sara@huhbub.com>
2016-10-23 21:14:16 +0100

    completed ch.9

---
+++ ch.9/ex2.rb
@@ -0,0 +1,14 @@
+#“Old-school Roman numerals. In the early days of Roman numerals, the Romans didn’t bother with any of this new-fangled subtraction “IX” nonsense. No sir, it was straight addition, biggest to littlest—so 9 was written “VIIII,” and so on. Write a method that when passed an integer between 1 and 3000 (or so) returns a string containing the proper old-school Roman numeral. In other words, old_roman_numeral 4 should return 'IIII'. Make sure to test your method on a bunch of different numbers. Hint: Use the integer division and modulus methods here.
+
+def oldroman_numeral num
+  roman = ''
+  roman = roman + 'M' * (num / 1000)
+  roman = roman + 'D' * (num % 1000 / 500)
+  roman = roman + 'C' * (num % 500 / 100)
+  roman = roman + 'L' * (num % 100 / 50)
+  roman = roman + 'X' * (num % 50 / 10)
+  roman = roman + 'V' * (num % 10 / 5)
+  roman = roman + 'I' * (num % 5 /1)
+  roman
+end
+puts(oldroman_numeral(1985))
